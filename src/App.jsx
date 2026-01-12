c62d1d152ee4f0380afd47e36ad2a893b196e4e6
princesinghrajput <prince.kumar@sensationsolutions.in>
2026-01-12 15:25:15 +0530

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
 import ComponentDocs from './pages/ComponentDocs'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <ComponentDocs />
+    // <ComponentDocs />
+
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ComponentDocs />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
