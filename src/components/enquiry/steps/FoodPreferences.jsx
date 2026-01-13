5e5b52117ecdf0b3a8567b5ae953dd66fcbc2acd
Jyoti <jyoti@sensationsolutions.in>
2026-01-13 16:29:29 +0530

    Revert "edrftgyhujmk"
    This reverts commit f2d44cf48c08d0d592adc2060dbb0a3194f0d939.

--- src/components/enquiry/steps/FoodPreferences.jsx
+++ src/components/enquiry/steps/FoodPreferences.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const FoodPreferences = () => {
     return (
         <>
-            FoodPreferencesfghj
+            cfghjbkml,FoodPreferenceshgghghgh
         </>
     );
 };
