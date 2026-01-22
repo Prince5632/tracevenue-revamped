e3858b98ae420b4fa1edcc4f91f686a7916ab536
Jyoti <jyoti@sensationsolutions.in>
2026-01-22 17:03:36 +0530

    new: wrap in auth provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@routes';
 import { ToastProvider } from './shared';
+import { AuthProvider } from '@/context/useAuthStore.jsx';
 
 /**
  * Main App component
  * Uses centralized router configuration from routes/index.jsx
  */
 function App() {
-  return <ToastProvider><RouterProvider router={router} /></ToastProvider>;
+  return (
+    <AuthProvider>
+      <ToastProvider>
+        <RouterProvider router={router} />
+      </ToastProvider>
+    </AuthProvider>
+  );
 }
 
 export default App;
+
