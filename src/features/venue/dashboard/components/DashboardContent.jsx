2f8ee30c6b6c873cb5a1b447f4a4a2d0e042f512
Rajindersingh-sensation <rajinder.singh@sensationsolutions.in>
2026-01-30 19:26:57 +0530

    restaurant-detail-view

--- src/features/venue/dashboard/components/DashboardContent.jsx
+++ src/features/venue/dashboard/components/DashboardContent.jsx
@@ -6,6 +6,10 @@ const DashboardContent = () => {
   return (
     <>
       <Outlet />
+      {/* <div className="fixed z-80">
+        <div>div1</div>
+        <div>div2</div>
+      </div> */}
     </>
   );
 };
