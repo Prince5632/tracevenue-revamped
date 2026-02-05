e8156dfdfe0ce14054470f85cc7179e6b88f37c2
Rajindersingh-sensation <rajinder.singh@sensationsolutions.in>
2026-02-05 11:51:51 +0530

    changes in active enquiry

--- src/features/venue/enquiry/shared/Header.jsx
+++ src/features/venue/enquiry/shared/Header.jsx
@@ -6,6 +6,8 @@ import { Button, Input } from '@/shared';
 import { useNavigate } from 'react-router-dom';
 import filters from "../../../../assets/dashboard/filters.png";
 import tabler from "../../../../assets/dashboard/tabler_list-details.png";
+import tick from "../../../../assets/images/tick.png";
+import { BsCheckLg } from "react-icons/bs";
 
 const Header = ({ heading, subheading, sortBy, setSortBy }) => {
     const [isOpen, setIsOpen] = useState(false);
@@ -44,19 +46,22 @@ const Header = ({ heading, subheading, sortBy, setSortBy }) => {
                     </div>
                 </div>
 
-                <div className='w-full h-1 bg-gray-200 my-4'></div>
+                <div className='relative h-1 my-4'>
+                    <div className='absolute h-1 bg-gray-200 left-0 right-1'></div>
+                </div>
 
                 {/* input fields */}
+                <div className='lg:pr-16'>
 
-                <div className='grid grid-cols-1 lg:grid-cols-[55%_35%_5%] gap-4 mt-3'>
+                 <div className='grid grid-cols-1 lg:grid-cols-[55%_40%_auto] gap-4 mt-3'>
                     <Input placeholder="Search enquiries" leftIcon={<img src={tabler} alt="tabler" />} />
 
-                    <div ref={inputRef} className='rounded-[30px] border border-[1px] border-[#D7D9DA] hover:border-orange-600
+                    <div ref={inputRef} className='relative rounded-[30px] border border-[1px] border-[#D7D9DA] hover:border-orange-600
                         cursor-pointer h-[47px] py-[10px] px-4'>
                         <div className='flex justify-between'>
-                            <div className='flex gap-2'>
+                            <div className='flex gap-2 whitespace-nowrap'>
                                 <div className='text-[16px] text-[#85878C] font-semibold'>Sort by:</div>
-                                <div className={`text-[16px] text-[#060606] font-semibold ${event === 'Latest Enquiries' ? "text-orange-600" : ""}`}>{event}</div>
+                                <div className={`text-[16px] text-orange-600 font-semibold`}>{event}</div>
                             </div>
 
                             <button className="cursor-pointer" onClick={() => {
@@ -92,22 +97,24 @@ const Header = ({ heading, subheading, sortBy, setSortBy }) => {
                             } */}
 
                             {isOpen &&
-                                <div className='bg-white h-[100px] rounded-[20px] border border-[1px] border-[#D7D9DA] absolute right-37 top-70 w-60 px-4 py-auto'>
-                                    <div className='flex justify-between items-center'>
+                                <div className='bg-white h-auto rounded-[20px] border border-[1px] border-[#D7D9DA] absolute right-0 top-11 w-60 px-5 py-2'>
+                                    <div className='flex justify-between items-center py-2'>
                                         <button onClick={() => {
                                             setEvent('Latest Enquiries')
                                             setSortBy('latest')
-                                        }} className='my-2 text-gray-600 font-semibold hover:text-orange-600 cursor-pointer'>Latest Enquiries</button>
+                                            setIsOpen(!isOpen)
+                                        }} className={` text-[#85878C] !text-[16px] !font-semibold cursor-pointer transition-transform duration-300 ease-in-out hover:translate-x-1  ${event === 'Latest Enquiries' ? "text-orange-600" : ""}`}>Latest Enquiries</button>
 
-                                        {/* {event === 'Latest Enquiries' && <img src="src\assets\images\tick.png" />} */}
+                                        {event === 'Latest Enquiries' && <BsCheckLg className='text-orange-600' />}
 
                                     </div>
-                                    <div className='flex justify-between items-center'>
+                                    <div className='flex justify-between items-center py-2'>
                                         <button onClick={() => {
                                             setEvent('Nearest Event date')
                                             setSortBy('nearest')
-                                        }} className='text-gray-600 font-semibold hover:text-orange-600 cursor-pointer'>Nearest Event date</button>
-                                        {/* {event === 'Nearest Event date' && <img src="src\assets\images\tick.png" />} */}
+                                            setIsOpen(!isOpen)
+                                        }} className={`text-[#85878C] !text-[16px] !font-semibold cursor-pointer transition-transform duration-300 ease-in-out hover:translate-x-1  ${event === 'Nearest Event date' ? "text-orange-600" : ""}`}>Nearest Event date</button>
+                                        {event === 'Nearest Event date' && <BsCheckLg className='text-orange-600' />}
 
                                     </div>
 
@@ -120,6 +127,7 @@ const Header = ({ heading, subheading, sortBy, setSortBy }) => {
                     <div className='w-[64px] h-[47px] border border-[1px] border-[#D7D9DA] rounded-[30px] px-5 py-[10px]'>
                         <img src={filters} alt="filters" />
                     </div>
+                 </div>
                 </div>
             </div>
 
