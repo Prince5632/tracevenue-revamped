e8fdaa7c4480ce25a387df52671eb5acd5a2a54a
taniya-sensation <taniya@sensationsolutions.in>
2026-02-11 15:21:21 +0530

    change padding

--- src/features/venue/enquiry/components/shared/ProgessHeader.jsx
+++ src/features/venue/enquiry/components/shared/ProgessHeader.jsx
@@ -4,7 +4,7 @@ const ProgressHeader = ({ currentStep, totalSteps, title, subtitle }) => {
   const progressPercentage = (currentStep / totalSteps) * 100;
 
   return (
-    <div className="w-full relative lg:left-0 px-4">
+    <div className="w-full relative lg:left-0 px-4 sm:px-0">
       <p className="text-[14px] font-semibold text-[#5C5F62] mb-1 hidden lg:block">
         Step {currentStep}/{totalSteps}
       </p>
