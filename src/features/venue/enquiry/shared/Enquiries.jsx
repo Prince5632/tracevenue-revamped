2d506a99431f37c27adccec944054b9d69409827
Rajindersingh-sensation <rajinder.singh@sensationsolutions.in>
2026-02-18 18:34:42 +0530

    searchBar-filter-functionality

--- src/features/venue/enquiry/shared/Enquiries.jsx
+++ src/features/venue/enquiry/shared/Enquiries.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { useDashboard } from "@/features/venue/dashboard/context/DashboardContext";
 import Header from './Header';
 import EnquiryCard from './EnquiryCard';
 
 const Enquiries = () => {
+  const [val, setVal] = useState('')
+  const [sortBy, setSortBy] = useState('latest');
   const { status } = useParams();
   console.log("Status",status);
 
@@ -34,15 +36,68 @@ const Enquiries = () => {
     }
   }, [jobs, apiStatus]);
 
+  // function to get preferred date
+  const getPreferredDate = (item) => {   // item = jobs.data[0]
+    return Object.keys(
+      item?.eventDateOptions?.preferredDates?.[0] || {}
+    )[0];
+  };
+
+  // function for sorting
+  const sortedData = useMemo(() => {
+    if (!jobs?.data) return [];
+
+    const dataCopy = [...jobs.data];
+
+    // Latest Enquiries (by createdAt)
+    if (sortBy === "latest") {
+      return dataCopy.sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt) // Negative->a , positive->b
+      );
+    }
+
+    // Nearest Event Date (by preferredDates)
+    if (sortBy === "nearest") {
+      const today = new Date();
+
+      return dataCopy
+        .filter((item) => {
+          const dateStr = getPreferredDate(item);
+          return dateStr && new Date(dateStr) >= today;
+        })
+        .sort((a, b) => {
+          const aDate = new Date(getPreferredDate(a));
+          const bDate = new Date(getPreferredDate(b));
+          return aDate - bDate;
+        });
+    }
+
+    return dataCopy;
+  }, [jobs, sortBy]);
+
+
+  // Function for filter the enquiry name
+  const filteredData = useMemo(() => {
+  if (!val) return sortedData;
+
+  return sortedData.filter((item) =>
+    item.name.toLowerCase().includes(val.toLowerCase())
+  );
+}, [sortedData, val]);
+
+
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error loading enquiries</div>;
 
+
+  
+
   return (
     <div>
-      <Header heading={status}/>
+      <Header heading={status} setSortBy={setSortBy} val={val} setVal={setVal}/>
 
-      {jobs?.data?.length>0 ? 
-      jobs?.data?.map((item)=>{
+      {filteredData.length>0 ? 
+      filteredData.map((item)=>{
         return <EnquiryCard key={item.id} data={item}/>
       }): <h1 className='mt-5 font-bold'>No {status} Enquiries</h1>
     }
